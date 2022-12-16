9323c452382f878ecfc7851d1f50042659d8f1cc
Guy Lazarof <lazarofguy111@gmail.com>
2022-12-16 22:44:08 +0200

    add grid to meme display

--- js/services/galleryService.js
+++ js/services/galleryService.js
@@ -14,29 +14,29 @@ var gImgs = [
   }, {
     id: 5, url: 'img/6.jpg', keywords: ['funny', 'science']
   },{
-    id: 6, url: 'img/7.jpg', keywords: ['funny', 'science']
+    id: 6, url: 'img/7.jpg', keywords: ['kid', 'surprise']
   },{
-    id: 7, url: 'img/8.jpg', keywords: ['funny', 'science']
+    id: 7, url: 'img/8.jpg', keywords: ['funny', 'listening']
   },{
-    id: 8, url: 'img/9.jpg', keywords: ['funny', 'science']
+    id: 8, url: 'img/9.jpg', keywords: ['funny', 'kid','bad']
   },{
-    id: 9, url: 'img/10.jpg', keywords: ['funny', 'science']
+    id: 9, url: 'img/10.jpg', keywords: ['obama', 'laugh']
   },{
-    id: 10, url: 'img/11.jpg', keywords: ['funny', 'science']
+    id: 10, url: 'img/11.jpg', keywords: ['fight', 'boys']
   },{
-    id: 11, url: 'img/12.jpg', keywords: ['funny', 'science']
+    id: 11, url: 'img/12.jpg', keywords: ['funny', 'man']
   },{
-    id: 12, url: 'img/13.jpg', keywords: ['funny', 'science']
+    id: 12, url: 'img/13.jpg', keywords: ['cheers', 'success']
   },{
-    id: 13, url: 'img/14.jpg', keywords: ['funny', 'science']
+    id: 13, url: 'img/14.jpg', keywords: ['surprise', 'matrix']
   },{
-    id: 14, url: 'img/15.jpg', keywords: ['funny', 'science']
+    id: 14, url: 'img/15.jpg', keywords: ['mad', 'zero','funny']
   },{
-    id: 15, url: 'img/16.jpg', keywords: ['funny', 'science']
+    id: 15, url: 'img/16.jpg', keywords: ['funny', 'laugh']
   },{
-    id: 16, url: 'img/17.jpg', keywords: ['funny', 'science']
+    id: 16, url: 'img/17.jpg', keywords: ['putin', 'war']
   },{
-    id: 17, url: 'img/18.jpg', keywords: ['funny', 'science']
+    id: 17, url: 'img/18.jpg', keywords: ['every where', 'toy']
   },
 ];
 
