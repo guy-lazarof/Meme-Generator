4872dc385bef8e8f8c2c45ff5307535218b20eb6
Guy Lazarof <lazarofguy111@gmail.com>
2022-12-17 15:26:40 +0200

    add input-file + download + share on faceBook

--- js/memeController.js
+++ js/memeController.js
@@ -124,6 +124,75 @@ function onTextAlignRight() {
   renderMeme()
 }
 
+function onShareMeme(){
+  
+}
+
+function onUploadImg(){
+  
+}
+
+function onDownloadMeme(){
+  
+}
+
+
+function onImgInput(ev) {
+  loadImageFromInput(ev, renderImg)
+}
+
+function loadImageFromInput(ev, onImageReady) {
+  const reader = new FileReader()
+  // After we read the file
+  reader.onload = (event) => {
+      let img = new Image() // Create a new html img element
+      img.src = event.target.result // Set the img src to the img file we read
+      // Run the callBack func, To render the img on the canvas
+      img.onload = () => onImageReady(img)
+  }
+
+  reader.readAsDataURL(ev.target.files[0]) // Read the file we picked
+
+}
+
+function renderImg(img) {
+  // Draw the img on the canvas
+  gCtx.drawImage(img, 0, 0, gElCanvas.width, gElCanvas.height)
+}
+
+
+function downloadCanvas(elLink) {
+  const data = gElCanvas.toDataURL()
+  elLink.href = data
+}
+
+function onUploadImg() {
+  const imgDataUrl = gElCanvas.toDataURL('image/jpeg') // Gets the canvas content as an image format
+
+  // A function to be called if request succeeds
+  function onSuccess(uploadedImgUrl) {
+      // Encode the instance of certain characters in the url
+      const encodedUploadedImgUrl = encodeURIComponent(uploadedImgUrl)
+      window.open(`https://www.facebook.com/sharer/sharer.php?u=${encodedUploadedImgUrl}&t=${encodedUploadedImgUrl}`)
+  }
+  // Send the image to the server
+  doUploadImg(imgDataUrl, onSuccess)
+}
+
+function doUploadImg(imgDataUrl, onSuccess) {
+  // Pack the image for delivery
+  const formData = new FormData()
+  formData.append('img', imgDataUrl)
+  console.log('formData:', formData)
+  // Send a post req with the image to the server
+  fetch('//ca-upload.com/here/upload.php', { method: 'POST', body: formData })
+      .then(res => res.text())
+      .then(url => {
+          console.log('url:', url)
+          onSuccess(url)
+      })
+}
+
 // function addListeners() {
 //   addMouseListeners()
 //   addTouchListeners()
